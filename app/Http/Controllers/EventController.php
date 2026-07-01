aa8b657afd605a299d131bdb69bac40f7a4aa7a7
muhfad <mfadhel@students.amikom.ac.id>
2026-07-02 00:21:42 +0700

    konfigurasi midtrans

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,22 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
-use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    public function show()
+    public function show(Event $event)
     {
-        return view('event-detail');
-    }
-
-    public function checkout()
-    {
-        return view('checkout');
-    }
-
-    public function ticket()
-    {
-        return view('ticket');
+        return view('events.show', compact('event'));
     }
 }
